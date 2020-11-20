61476d376a56b5152c4423890045a6af003e42ec
Chona <chonisimo@hotmail.com>
2020-11-20 15:17:29 -0300

    Version con alertas

--- routes/carrito.js
+++ routes/carrito.js
@@ -4,8 +4,29 @@ const sqlite = require('sqlite3');
 
 const db = new sqlite.Database('./BoinaEditorialLibros3.sqlite');
 
+carritoRouter.get('/', (req, res, next) => {
+    db.all('SELECT * FROM Libros', (err, libros) => {
+        if(err) {
+            next(err);
+        } else {
+            res.render('carrito/carrito', {libros: libros.map(libro => {
+                return libro;
+            })});
+        }
+    });
+});
+
 carritoRouter.post('/', (req, res, next) => {
-    
-})
+    const tapa = req.body.libros.tapa;
+    const titulo = req.body.libros.titulo;
+    const precio = req.body.libros.precio;
+    db.get('SELECT * FROM Libros WHERE Libros.id = $librosId', {$librosId: req.body.libros.id}, (err, libros) => {
+        if(err) {
+            next(err);
+        } else {
+            res.send({libros: libros});
+        }
+    });
+});
 
 module.exports = carritoRouter;
--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use('/public', express.static('public'));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
 app.use('/', indexRouter);
 app.use('/catalogo', catalogoRouter);
