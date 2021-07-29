ae433df4426d8b17b635909d15eda982901cbcd6
Chona <chonisimo@hotmail.com>
2021-07-28 21:10:40 -0300

    catalogo hecho

--- routes/index.js
+++ routes/index.js
@@ -23,36 +23,7 @@ indexRouter.param('librosId', (req, res, next, librosId) => {
 //Muestra la pagina de index
 indexRouter.get('/', (req, res, next) => {
     db.all('SELECT * FROM Libros', (err, libros) => {
-        if(err) {
-            next(err);
-        } else {
-            res.render('index', {libros: libros.map(libro => {
-                return libro;
-            }), carro: carro.length});
-        }
-    });
-});
-
-//Muestra la pagina del carrito
-indexRouter.get('/carrito', (req, res, next) => {
-    db.all('SELECT * FROM Libros', (err, libros) => {
-        if(err) {
-            next(err);
-        } else {
-            res.render('carrito/carrito', {libros: carro || libros, carro: carro.length});
-        }
-})});
-
-//agrega libro al carrito en su array carro
-indexRouter.post('/carrito', (req, res, next) => {
-    const identificacion = req.body.identificacion;
-    db.get('SELECT * FROM Libros WHERE Libros.id = $librosId', {$librosId: identificacion}, (err, libros) => {
-        if(err) {
-            next(err);
-        } else {
-            carro.push(libros);
-            res.render('catalogo/libro', {libros: libros, carro: carro.length});
-        }
+        res.render('index');
     });
 });
 
@@ -93,12 +64,52 @@ indexRouter.get('/catalogo/libro/:librosId', (req, res, next) => {
     });
 });
 
-//El aviso de compra realizada
-indexRouter.get('/carrito/compra', (req, res, next) => {
-    do {
-        carro.pop()
-    } while (carro.length > 0);
-    res.render('carrito/compra', {carro: carro.length});
+//muestra la pagina de artistas
+indexRouter.get('/artistas', (req, res, next) => {
+    db.all('SELECT * FROM Libros', (err, libros) => {
+        if(err) {
+            next(err);
+        } else {
+            res.render('artistas/artistas', {libros: libros.map(libro => {
+                return libro;
+            })})
+        }
+    });
+});
+
+//muestra la pagina de suscripciones
+indexRouter.get('/suscripciones', (req, res, next) => {
+    res.render('suscripciones/suscripciones');
+});
+
+//muestra la pagina de suscripciones nivel 1
+indexRouter.get('/suscripciones/nivel1', (req, res, next) => {
+    res.render('suscripciones/nivel1');
+});
+
+//muestra la pagina de suscripciones nivel 2
+indexRouter.get('/suscripciones/nivel2', (req, res, next) => {
+    res.render('suscripciones/nivel2');
+});
+
+//muestra la pagina de suscripciones nivel 1
+indexRouter.get('/suscripciones/nivel3', (req, res, next) => {
+    res.render('suscripciones/nivel3');
+});
+
+//muestra la pagina de suscripciones nivel 1
+indexRouter.get('/suscripciones/nivel4', (req, res, next) => {
+    res.render('suscripciones/nivel4');
+});
+
+//muestra la pagina de suscripciones nivel 1
+indexRouter.get('/comiquerias', (req, res, next) => {
+    res.render('comiquerias/comiquerias');
+});
+
+//muestra la pagina de suscripciones nivel 1
+indexRouter.get('/contacto', (req, res, next) => {
+    res.render('contacto/contacto');
 });
 
 module.exports = indexRouter;
